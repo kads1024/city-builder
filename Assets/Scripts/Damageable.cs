c50ca5d432add2c63d5772518f517c41c0f94b98
Kenneth Amiel Santos <dg.kennethsantos@gmail.com>
2021-02-10 20:47:16 +0800

    Added GameEvent Utility

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Component that has health and can be damaged
+/// </summary>
+public class Damageable : MonoBehaviour
+{
+    // Game Events
+    [SerializeField] private GameEvent _onDestroy;
+    [SerializeField] private GameEvent _onHit;
+
+    // Health
+    [SerializeField] private int _totalHealth = 100;
+    private int _currentHealth;
+
+    // Initialization
+    private void Start()
+    {
+        _currentHealth = _totalHealth;
+    }
+
+    /// <summary>
+    /// Deduct the health of the object then raise the events that will happen when the object is hit.
+    /// Check if the object has no more health so it can be removed from the scene
+    /// </summary>
+    /// <param name="p_damage"></param>
+    public void Hit(int p_damage)
+    {
+        _onHit.Raise();
+        _currentHealth -= p_damage;
+
+        if (_currentHealth <= 0)
+            Destroy();
+    }
+
+    /// <summary>
+    /// Raise any Destroy event before destroying this object from the scene
+    /// </summary>
+    private void Destroy()
+    {
+        _onDestroy.Raise();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Utilities/GameEvent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Game Event is a scriptable object that serves as a channel to raise events in a more modular manner
+/// </summary>
+[CreateAssetMenu(menuName = "Events/Void Game Event")]
+public class GameEvent : ScriptableObject
+{
+    private List<GameEventListener> _listeners = new List<GameEventListener>();
+
+    /// <summary>
+    /// Invoke All Listeners registered to this event
+    /// </summary>
+    public void Raise()
+    {
+        for (int i = _listeners.Count - 1; i >= 0; i--) {
+            _listeners[i].OnEventRaised();
+        }
+    }
+
+    /// <summary>
+    /// Add a certain listener for the event 
+    /// </summary>
+    /// <param name="p_listener">The Listener that will be added</param>
+    public void RegisterListener(GameEventListener p_listener)
+    {
+        _listeners.Add(p_listener);
+    }
+
+    /// <summary>
+    /// Remove a certain listener for the event 
+    /// </summary>
+    /// <param name="p_listener">The Listener that will be removed</param>
+    public void UnregisterListener(GameEventListener p_listener)
+    {
+        _listeners.Remove(p_listener);
+    }
+}
