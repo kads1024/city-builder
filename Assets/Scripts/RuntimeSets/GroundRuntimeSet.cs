2f79a74c1506f6a3d7b0ad957f49ba3c151b4e88
Kenneth Amiel Santos <dg.kennethsantos@gmail.com>
2021-02-10 21:34:49 +0800

    Added Runtime Sets

---
+++ Assets/Scripts/RuntimeSets/GroundRuntimeSet.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Runtime set for tracking the number of grounds in the scene
+/// </summary>
+[CreateAssetMenu(menuName = "Runtime Sets/Ground")]
+public class GroundRuntimeSet : RuntimeSet<Ground> {}
---
+++ Assets/Scripts/Utilities/RuntimeSet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Keeps Track of a set of objects in the scene during runtime
+/// </summary>
+/// <typeparam name="T">The type of object to be tracked</typeparam>
+public class RuntimeSet<T> : ScriptableObject
+{
+    // List of all the objects in the scene
+    public List<T> Items = new List<T>();
+
+    /// <summary>
+    /// Adds the item to the list
+    /// </summary>
+    /// <param name="p_item">Item to be added</param>
+    public void Add(T p_item)
+    {
+        if (!Items.Contains(p_item)) Items.Add(p_item);
+    }
+
+    /// <summary>
+    /// Removes the item to the list
+    /// </summary>
+    /// <param name="p_item">Item to be removed</param>
+    public void Remove(T p_item)
+    {
+        if (Items.Contains(p_item)) Items.Remove(p_item);
+    }
+}
