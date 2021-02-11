acd9f333b601f164022268c96b81f9b3e42af667
Kenneth Amiel Santos <dg.kennethsantos@gmail.com>
2021-02-11 19:51:51 +0800

    Added Camera Zoom and Updated Camera movement

--- Assets/Scripts/Miscellaneous/CameraController.cs
+++ Assets/Scripts/Miscellaneous/CameraController.cs
@@ -14,11 +14,18 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float _minZoomDistance;
     [SerializeField] private float _maxZoomDistance;
 
-    private Camera _camera;
+    private Transform _zoomObject;
+    private Transform _cameraTransform;
 
     private void Awake()
     {
-        _camera = Camera.main;
+        _zoomObject = transform.GetChild(0);
+        _cameraTransform = Camera.main.transform;
+    }
+
+    private void Start()
+    {
+        transform.LookAt(_cameraTransform);
     }
 
     private void Update()
@@ -29,20 +36,32 @@ public class CameraController : MonoBehaviour
 
     private void Move()
     {
-        Vector3 direction = _input.CameraDirection();
-        transform.position += Time.deltaTime * _moveSpeed * direction;
+        // Keyboard input
+        Vector3 moveInput = _input.CameraDirection();
+
+        // Mouse Position Horizontal
+        Vector2 mousePos = Input.mousePosition;
+        if (mousePos.x > Screen.width * 0.95f && mousePos.x < Screen.width)
+            moveInput.x = 1.0f;
+        else if (mousePos.x < Screen.width * 0.05f && mousePos.x > 0.0f)
+            moveInput.x = -1.0f;
+        // Mouse Position Vertical
+        if (mousePos.y > Screen.height * 0.95f && mousePos.y < Screen.height)
+            moveInput.z = 1.0f;
+        else if (mousePos.y < Screen.height * 0.05f && mousePos.y > 0.0f)
+            moveInput.z = -1.0f;
+
+        // Movement
+        Vector3 movementDirection = _cameraTransform.TransformDirection(moveInput);
+        movementDirection.y = 0;
+        transform.position += movementDirection.normalized * Time.deltaTime * _moveSpeed;
     }
 
     private void Zoom()
     {
         float scrollInput = _input.MouseScroll();
-        float cameraDistance = Vector3.Distance(transform.position, _camera.transform.position);
 
-        if (cameraDistance <= _minZoomDistance && scrollInput > 0.0f)
-            return;
-        else if (cameraDistance >= _maxZoomDistance && scrollInput < 0.0f)
-            return;
-
-        _camera.transform.position += _camera.transform.forward * scrollInput * _zoomSpeed;
+        _zoomObject.localPosition += new Vector3(0, 0, -scrollInput * _zoomSpeed);
+        _zoomObject.localPosition = new Vector3(_zoomObject.localPosition.x, _zoomObject.localPosition.y, Mathf.Clamp(_zoomObject.localPosition.z, _minZoomDistance, _maxZoomDistance));
     }
 }
