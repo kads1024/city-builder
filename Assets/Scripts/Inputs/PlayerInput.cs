f22948dcba551e22927b0b799ba12ca1cd2d7e65
Kenneth Amiel Santos <dg.kennethsantos@gmail.com>
2021-02-11 14:46:39 +0800

    Changed Player Input to be a Scriptable Object

--- Assets/Scripts/Inputs/PlayerInput.cs
+++ Assets/Scripts/Inputs/PlayerInput.cs
@@ -3,17 +3,46 @@
 /// <summary>
 /// Player Input Helps in modifying input for the player
 /// </summary>
-public class PlayerInput : MonoBehaviour
+[CreateAssetMenu(menuName = "Inputs/Player Input")]
+public class PlayerInput : ScriptableObject
 {
-    private enum MouseButton {
+    private enum MouseButton
+    {
         LeftMouseButton,
         RightMouseButton
     }
 
+    private const string MOUSE_SCROLL_WHEEL = "Mouse ScrollWheel";
+
     // Options for selecting the mouse input
+    [Header("Mouse Buttons")]
     [SerializeField] private MouseButton _selectionButton;
     [SerializeField] private MouseButton _interactionButton;
 
+    // Options for selecting the mouse input
+    [Header("Keyboard Buttons")]
+    [SerializeField] private KeyCode _cameraUp;
+    [SerializeField] private KeyCode _cameraDown;
+    [SerializeField] private KeyCode _cameraLeft;
+    [SerializeField] private KeyCode _cameraRight;
+
+    /// <summary>
+    /// Camera Movement Controls
+    /// </summary>
+    /// <returns>The normalized direction where the camera will move</returns>
+    public Vector3 CameraDirection()
+    {
+        Vector3 direction = Vector3.zero;
+
+        if (Input.GetKey(_cameraUp)) direction += Vector3.forward;
+        if (Input.GetKey(_cameraDown)) direction += Vector3.back;
+        if (Input.GetKey(_cameraLeft)) direction += Vector3.left;
+        if (Input.GetKey(_cameraRight)) direction += Vector3.right;
+
+        return direction.normalized;
+    }
+
+    // Mouse Buttons
     #region CLICK
     public bool OnSelectionClick()
     {
@@ -47,6 +76,13 @@ public class PlayerInput : MonoBehaviour
     public bool OnInteractionUp()
     {
         return Input.GetMouseButtonUp((int)_interactionButton);
+    }
+    #endregion
+
+    #region SCROLL
+    public float MouseScroll()
+    {
+        return Input.GetAxis(MOUSE_SCROLL_WHEEL);
     }
     #endregion
 }
