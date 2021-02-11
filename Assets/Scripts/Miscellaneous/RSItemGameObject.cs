851215060669cfd50a92f1b6d688743721730da2
Kenneth Amiel Santos <dg.kennethsantos@gmail.com>
2021-02-11 14:47:44 +0800

    Changed Ground RS to a more general GameObjectRS

---
+++ Assets/Scripts/Miscellaneous/RSItemGameObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A single item that will be added to the GameObjectRuntimeSet
+/// </summary>
+public class RSItemGameObject : MonoBehaviour
+{
+    // The Set where we will add this object tos
+    [SerializeField] private GameObjectRuntimeSet _gameObjectSet;
+
+    // Add whenever avaiable
+    private void OnEnable()
+    {
+        _gameObjectSet.Items.Add(gameObject);
+    }
+
+    // Remove when disabled to avoid unecessary tracking
+    private void OnDisable()
+    {
+        _gameObjectSet.Items.Remove(gameObject);
+    }
+}
---
+++ Assets/Scripts/RuntimeSets/GameObjectRuntimeSet.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Runtime Set for tracking Game Objects Objects
+/// </summary>
+[CreateAssetMenu(menuName = "Runtime Sets/Game Object Set")]
+public class GameObjectRuntimeSet : RuntimeSet<GameObject> {}
