375c822b2f65456c7e7998e4db9b120feddf4d4b
Kenneth Amiel Santos <dg.kennethsantos@gmail.com>
2021-02-12 02:22:31 +0800

    Modified Selectables

---
+++ Assets/Scripts/SelectedObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Script to be attached to the object that is currently selected
+/// </summary>
+public class SelectedObject : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+}
