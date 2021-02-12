e4088e201cbd177f6270d12012dc18c4bf721e6c
Kenneth Amiel Santos <dg.kennethsantos@gmail.com>
2021-02-12 13:38:07 +0800

    Added Animation Event Listener and Event Callback for those listeners

---
+++ Assets/Scripts/AnimationEventListener.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Acts as a bridge to animation events
+/// </summary>
+public class AnimationEventListener : MonoBehaviour
+{
+    // List of animation events to call when an animation event is called
+    [SerializeField] private GameEvent _footStepEvent;
+    [SerializeField] public GameEvent _attackEvent;
+
+    public void FootstepEvent()
+    {
+        _footStepEvent.Raise();
+    }
+
+    public void AttackEvent()
+    {
+        _attackEvent.Raise();
+    }
+}
